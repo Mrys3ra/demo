cfc2daa21bbe22f54a3039675ca009a22e05ba57
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-09 20:22:11 +0800

    Update instance0f.js

--- instance0f.js
+++ instance0f.js
@@ -2,8 +2,12 @@ class Animal {}
 
 class Dog extends Animal {}
 
-let dog = new Dog();
+class Cat{}
 
-console.log(dog instanceof Dog); // Output: true - 'dog' is an instance of Dog
+let dog = new Dog();
+let cat = new Cat();
+
+console.log(dog instanceof Cat); // Output: false - 'dog' is not an instance of cat
 console.log(dog instanceof Animal); // Output: true - 'dog' is also an instance of Animal
-console.log(dog instanceof Object); // Output: true - 'dog' is an instance of Object (since all objects inherit from Object)
+console.log(dog instanceof Object);// Output: true - 'dog' is an instance of Object (since all objects inherit from Object)
+console.log(cat instanceof Object);// Output: true - 'cat' is an instance of Object (since all objects inherit from Object)
