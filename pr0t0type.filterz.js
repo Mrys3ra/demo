4ed4e25f02ff6a1db2805b7df17683f2547b7f17
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-09 20:57:34 +0800

    Create pr0t0type.filterz.js

---
+++ pr0t0type.filterz.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5, 6];
+
+// Filter elements greater than 3
+let greaterThanThree = numbers.filter(function(element) {
+  return element > 3;
+});
+
+console.log(greaterThanThree); // Output: [4, 5, 6]
