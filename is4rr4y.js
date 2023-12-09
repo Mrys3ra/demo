4976a202ff72f37de2de266627970d03b50612f4
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-09 20:04:20 +0800

    Create is4rr4y.js

---
+++ is4rr4y.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3];
+let notArr = "Hello";
+
+console.log(Array.isArray(arr)); // Output: true - 'arr' is an array
+console.log(Array.isArray(notArr)); // Output: false - 'notArr' is not an array
