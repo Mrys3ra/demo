fd365377cb4d0c1ddfda0ee0553aa160a9c3c25c
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-09 20:46:06 +0800

    Create pr0t0type.s0me.js

---
+++ pr0t0type.s0me.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5];
+
+// Check if at least one element is greater than 3
+let anyGreaterThanThree = numbers.some(function(element) {
+  return element > 3;
+});
+
+console.log(anyGreaterThanThree); // Output: true (at least one element is greater than 3)
