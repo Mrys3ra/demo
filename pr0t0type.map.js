da0bf025cad2669ce012633275028251e3b5fa49
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-09 21:02:49 +0800

    Create pr0t0type.map.js

---
+++ pr0t0type.map.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5];
+
+// Map each element to its square
+let squaredNumbers = numbers.map(function(element) {
+  return element * element;
+});
+
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
