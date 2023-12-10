eb3a07b71d78d73a404ac053c35b239c779281fe
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-10 10:49:07 +0800

    Create arry+object.js

---
+++ arry+object.js
@@ -0,0 +1,11 @@
+//array - use indices
+let myArray = ['apple', 'banana', 'orange'];
+console.log(myArray[0]); // Output: 'apple'
+
+//object - use string
+let myObject = {
+  fruit1: 'apple',
+  fruit2: 'banana',
+  fruit3: 'orange'
+};
+console.log(myObject['fruit1']); // Output: 'apple'
