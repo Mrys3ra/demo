4168465876bd052cbe900c2ae5cea5158ad28a53
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-10 12:26:15 +0800

    Create objectentries.js

---
+++ objectentries.js
@@ -0,0 +1,10 @@
+//objectentries
+let person = {
+  name: 'Alice',
+  age: 30,
+  city: 'New York'
+};
+
+let entries = Object.entries(person);
+console.log(entries);
+// Output: [['name', 'Alice'], ['age', 30], ['city', 'New York']]
