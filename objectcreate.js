7edcc043dbd04d2e1bfba8a078329fb67d34cf66
Mrys3ra <114344803+Mrys3ra@users.noreply.github.com>
2023-12-10 12:11:16 +0800

    Create objectcreate.js

---
+++ objectcreate.js
@@ -0,0 +1,12 @@
+// Creating a person object prototype
+let personProto = {
+  greet: function() {
+    return `Hello, ${this.name}!`;
+  }
+};
+
+// Creating a new object 'alice' with 'personProto' as its prototype
+let alice = Object.create(personProto);
+alice.name = 'Alice';
+
+console.log(alice.greet()); // Output: 'Hello, Alice!'
